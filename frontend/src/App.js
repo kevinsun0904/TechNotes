2323ab7aeae9188cbf168c4e8658373a1b7b218c
kevinsun0904 <122562552+kevinsun0904@users.noreply.github.com>
2024-03-29 18:47:41 -0400

    Implemented react and basic homepage layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Public from './components/Public'
-import Login from './features/auth/Login';
+import Login from './features/auth/Login'
 import DashLayout from './components/DashLayout'
 import Welcome from './features/auth/Welcome'
 import NotesList from './features/notes/NotesList'
@@ -15,7 +15,7 @@ function App() {
         <Route path="login" element={<Login />} />
 
         <Route path="dash" element={<DashLayout />}>
-
+          
           <Route index element={<Welcome />} />
 
           <Route path="notes">
